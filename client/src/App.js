2497b961dfa1afd2cb8a7b80c34361735ff42046
jeffersondenilson <jefersond121212@gmail.com>
2020-08-15 19:51:30 -0300

    replacing drawer with appbar

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import Drawer from '@material-ui/core/Drawer';
 import Divider from '@material-ui/core/Divider';
 import Hidden from '@material-ui/core/Hidden';
@@ -14,14 +15,15 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import AddIcon from '@material-ui/icons/Add';
 import SortIcon from '@material-ui/icons/Sort';
 import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import SearchIcon from '@material-ui/icons/Search';
 import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
 import Task from './Task';
 
@@ -46,76 +48,67 @@ let tasks = [
 	},
 ];
 
-const sortOptions = ['title', 'complete', 'modified'];
-const drawerWidth = 250;
+const sortOptions = ['title', 'completed', 'modified'];
+// const drawerWidth = 250;
 const styles = theme => ({
-  /*
-  root: {
-		display: "flex",
-		flexGrow: 1,
-		margin: "10px 5px 0 5px",
-		[theme.breakpoints.up('sm')]: {
-			marginTop: "20px",
-			marginLeft: "20px"
-		}
-  },
-  appBar: {
-  	color: "black",
-		backgroundColor: "rgba(255, 255, 255, 0.7)",
-		// marginBottom: theme.spacing(3),
-		paddingLeft: theme.spacing(3)
-	},
-	drawer:{
-		width: "200px"
-	},
-	toolbar: theme.mixins.toolbar,
-  tasksContainer: {
-		display: "flex",
-		flexDirection: "column",
-		marginLeft: theme.spacing(3)
-  	// [theme.breakpoints.down('md')]: {
-  	// 	flexDirection: "column"
-  	// }
-  }
-  */
   root: {
     display: 'flex',
   },
-  drawer: {
-    [theme.breakpoints.up('sm')]: {
-      width: drawerWidth,
-      flexShrink: 0,
-    },
-  },
+  // drawer: {
+  //   [theme.breakpoints.up('sm')]: {
+  //     width: drawerWidth,
+  //     flexShrink: 0,
+  //   },
+  // },
   appBar: {
   	color: "black",
-		backgroundColor: "rgba(255, 255, 255, 0.7)",
+		backgroundColor: "rgba(255, 255, 255, 0.6)"
 		// zIndex: theme.zIndex.drawer + 1,
-    [theme.breakpoints.up('sm')]: {
-      width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: drawerWidth,
-    },
+    // [theme.breakpoints.up('sm')]: {
+    //   width: `calc(100% - ${drawerWidth}px)`,
+    //   marginLeft: drawerWidth,
+    // },
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up('sm')]: {
-      display: 'none',
-    },
+  appTitle: {
+  	marginRight: 40,
+  	[theme.breakpoints.down('xs')]: {
+  		display: 'none'	
+  	}
   },
+  button: {
+    // margin: theme.spacing(1)
+    // marginLeft: '2px',
+    // marginRight: '2px'
+  },
+  actions: {
+  	[theme.breakpoints.down('xs')]: {
+      display: 'flex',
+	  	flexDirection: 'row',
+	  	alignItems: 'center',
+	  	justifyContent: 'space-between'
+    },
+  	
+  },
+  // menuButton: {
+  //   marginRight: theme.spacing(2),
+  //   [theme.breakpoints.up('sm')]: {
+  //     display: 'none',
+  //   },
+  // },
   // necessary for content to be below app bar
   toolbar: {
   	...theme.mixins.toolbar,
-  	fontWeight: "bold", 
-  	fontSize: "1.5rem",
-  	color: "#4d4d4d",
-  	display: "flex",
-  	alignItems: "center",//vertical
-  	justifyContent: "space-between",//horizontal,
-  	marginLeft: 50
+  	// fontWeight: "bold", 
+  	// fontSize: "1.5rem",
+  	// color: "#4d4d4d",
+  	// display: "flex",
+  	// alignItems: "center",//vertical
+  	// justifyContent: "space-between",//horizontal,
+  	// marginLeft: 50
   },
-  drawerPaper: {
-    width: drawerWidth
-  },
+  // drawerPaper: {
+  //   width: drawerWidth
+  // },
   content: {
     flexGrow: 1,
    	padding: theme.spacing(1),
@@ -135,7 +128,6 @@ class App extends React.Component {
 		super(props);
 		this.state = { 
 			tasks,
-			drawerOpen: false,
 			sort: 1,
 			ascendingOrder: true
 		};
@@ -155,84 +147,85 @@ class App extends React.Component {
 
   render (){
   	const { classes } = this.props;
-		const { tasks, drawerOpen, sort, ascendingOrder } = this.state;
+		const { tasks, sort, ascendingOrder } = this.state;
 
     return(
     	<div className={classes.root}>
 	      <AppBar className={classes.appBar} position="fixed">
-	        <Toolbar>
+	        <Toolbar className={classes.actions}>
+	        	<Typography className={classes.appTitle} variant="h6" noWrap>
+	        		To Do App
+	        	</Typography>
+	        	{/*<span className={classes.buttons}>*/}
 		        <IconButton
-	            color="inherit"
-	            aria-label="open drawer"
-	            edge="start"
-	            onClick={this.handleDrawerToggle}
-	            className={classes.menuButton}
-	          >
-	            <MenuIcon />
+	            aria-label="add task"
+		          edge="start"
+			        color="inherit"
+			        className={classes.button}
+			      >
+	            <AddIcon />
 	          </IconButton>
+
+	          {/*<Button
+	            // variant="outlined"
+			        color="inherit"
+			        className={classes.button}
+			        startIcon={<AddIcon />}
+	          >
+	            Add
+	          </Button>*/}
+
+	          <Button
+	            // variant="outlined"
+			        color="inherit"
+			        className={classes.button}
+			        startIcon={<SortIcon />}
+	          >
+	            {sortOptions[sort]}
+	          </Button>
+
+	          { 
+	          	ascendingOrder ? 
+	          	<Button
+		            // variant="outlined"
+				        color="inherit"
+				        className={classes.button}
+				        startIcon={<ArrowDropUpIcon />}
+		          >
+		            ASC
+		          </Button>
+		          :
+		          <Button
+		            variant="outlined"
+				        color="inherit"
+				        className={classes.button}
+				        startIcon={<ArrowDropDownIcon />}
+		          >
+		            DESC
+		          </Button>
+	        	}
+	        	
+	        	<Hidden smUp>
+		          <IconButton
+		            color="inherit"
+		            aria-label="search task"
+		            edge="start"
+		            // onClick={this.handleDrawerToggle}
+		            className={classes.button}
+		            // style={{ marginLeft: 'auto' }}
+		          >
+		            <SearchIcon />
+		          </IconButton>
+	          </Hidden>
+	          {/*</span>*/}
 	        </Toolbar>
 	      </AppBar>
-	      
-				<nav className={classes.drawer}>
-					{/*mobile drawer*/}
-					<Hidden smUp implementation="css">
-	          <Drawer
-	            // container={container}
-	            variant="temporary"
-	            // anchor={theme.direction === 'rtl' ? 'right' : 'left'}
-	            open={drawerOpen}
-	            onClose={this.handleDrawerToggle}
-	            classes={{
-	              paper: classes.drawerPaper,
-	            }}
-	            ModalProps={{
-	              keepMounted: true,
-	            }}
-	          >
-	          	<div className={classes.toolbar}>
-	          		<span>To Do App</span>
-	          		<IconButton
-			            color="inherit"
-			            aria-label="close drawer"
-			            edge="start"
-			            onClick={this.handleDrawerToggle}
-			          >
-			            <ChevronLeftIcon />
-			          </IconButton>
-	          	</div>
-	          	<Divider />
-	            <DrawerList 
-	            	sort={sort} 
-	            	ascendingOrder={ascendingOrder}
-	            	toggleAscendingOrder={this.toggleAscendingOrder}
-	            />
-	          </Drawer>
-	        </Hidden>
-	      	{/*desktop drawer*/}
-	        <Hidden xsDown implementation="css">
-	          <Drawer
-	            classes={{
-	              paper: classes.drawerPaper,
-	            }}
-	            variant="permanent"
-	            open
-	          >
-	          	<div className={classes.toolbar}>To Do App</div>
-	          	<Divider />
-	            <DrawerList 
-	            	sort={sort} 
-	            	ascendingOrder={ascendingOrder}
-	            	toggleAscendingOrder={this.toggleAscendingOrder}
-	            />
-	          </Drawer>
-	        </Hidden>
-					</nav>
 
-					<main className={classes.content}>
-						<div className={classes.toolbar} />
-	      		{ tasks.map( task => 
-	      		<Task key={task._id} task={task} handleChange={this.handleChange} /> ) }
-					</main>
+				<main className={classes.content}>
+					<div className={classes.toolbar} />
+      		{ tasks.map( task => 
+      		<Task key={task._id} task={task} handleChange={this.handleChange} /> ) }
+				</main>
 			</div>
     );
   }
